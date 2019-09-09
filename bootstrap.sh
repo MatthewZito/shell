7a5a340a2935deec29443920ae9b1b7a91966ae3
Matthew Zito <47864657+MatthewZito@users.noreply.github.com>
2019-09-09 14:41:04 -0700

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+function doIt() {
+    rsync --exclude ".git/" \
+        --exclude ".DS_Store" \
+        --exclude "bootstrap.sh" \
+        --exclude "README.md" \
+        --exclude ".editorconfig" \
+        -avh --no-perms . ~;
+    source ~/.bash_profile;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    doIt;
+else
+    read -p "This may overwrite existing files in the home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt;
+    fi;
+fi;
+unset doIt;
